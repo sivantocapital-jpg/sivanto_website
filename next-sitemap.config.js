acb558669409b6d1bcdea5b3a204a1973fb2b582
sivantocapital-jpg <sivantocapital@gmail.com>
2026-02-09 23:26:43 +0530

    Add next-sitemap and config for Google Search Console

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.sivantocapital.in',  // your main site URL
+  generateRobotsTxt: true,                  // optional but recommended
+  sitemapSize: 5000,                         // optional, for larger sites
+};
