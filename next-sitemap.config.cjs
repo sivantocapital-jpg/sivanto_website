9ff75d26b335c8736d2801f32022944aa47571a4
sivantocapital-jpg <sivantocapital@gmail.com>
2026-02-09 23:47:47 +0530

    Add sitemap and robots.txt for Google Search Console

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -2,5 +2,5 @@
 module.exports = {
   siteUrl: 'https://www.sivantocapital.in',  // your main site URL
   generateRobotsTxt: true,                  // optional but recommended
-  sitemapSize: 5000,                         // optional, for larger sites
+  
 };
